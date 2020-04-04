ea5dd178e7b9040902a84f3aed38fe3ac0e381b5
Johannes Priebsch <developer@priebsch.at>
2020-04-04 11:34:24 +0200

    update names to score... and name...

--- strawberry-fields/src/main/java/at/wifiwien/javaswe/strawberry_fields/controller/GameInfoController.java
+++ strawberry-fields/src/main/java/at/wifiwien/javaswe/strawberry_fields/controller/GameInfoController.java
@@ -14,16 +14,16 @@ public class GameInfoController {
     private URL location;
 
     @FXML
-    private Label player1Label;
+    private Label namePlayer1Label;
 
     @FXML
-    private Label strawberriesPlayer1Label;
+    private Label scorePlayer1Label;
 
     @FXML
-    private Label player2Label;
+    private Label namePlayer2Label;
 
     @FXML
-    private Label strawberriesPlayer2Label;
+    private Label scorePlayer2Label;
 
     @FXML
     private Label strawberriesLeftLabel;
@@ -33,12 +33,26 @@ public class GameInfoController {
 
     @FXML
     void initialize() {
-        assert player1Label != null : "fx:id=\"player1Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
-        assert strawberriesPlayer1Label != null : "fx:id=\"strawberriesPlayer1Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
-        assert player2Label != null : "fx:id=\"player2Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
-        assert strawberriesPlayer2Label != null : "fx:id=\"strawberriesPlayer2Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
+        assert namePlayer1Label != null : "fx:id=\"player1Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
+        assert scorePlayer1Label != null : "fx:id=\"strawberriesPlayer1Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
+        assert namePlayer2Label != null : "fx:id=\"player2Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
+        assert scorePlayer2Label != null : "fx:id=\"strawberriesPlayer2Label\" was not injected: check your FXML file 'GameInfo.fxml'.";
         assert strawberriesLeftLabel != null : "fx:id=\"strawberriesLeftLabel\" was not injected: check your FXML file 'GameInfo.fxml'.";
         assert numberStrawberriesLeftLabel != null : "fx:id=\"numberStrawberriesLeftLabel\" was not injected: check your FXML file 'GameInfo.fxml'.";
 
+        initUIElements();
     }
+
+	private void initUIElements() {
+	
+		// set player names
+		namePlayer1Label.setText("Jan" + ":");
+		namePlayer2Label.setText("Birgit" + ":");
+		
+		// set game scores
+		scorePlayer1Label.setText("0");
+		scorePlayer2Label.setText("0");
+		numberStrawberriesLeftLabel.setText("20");
+		
+	}
 }
